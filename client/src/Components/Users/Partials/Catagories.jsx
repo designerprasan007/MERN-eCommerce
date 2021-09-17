4b007baf4b21094e2f94a2d88e56f0b7dd08fbf0
Codebug <prasanna2904>
2021-09-17 14:56:22 +0530

    added catagory

--- client/src/Components/Users/Partials/Catagories.jsx
+++ client/src/Components/Users/Partials/Catagories.jsx
@@ -1,6 +1,39 @@
+import tvimg from '../../../Assets/images/tv.jpeg';
+import fridge from '../../../Assets/images/fridge.jpeg';
+import laptopimg from '../../../Assets/images/laptop.png';
+import mobileimg from '../../../Assets/images/mobile.jpeg';
+import fashionimg from '../../../Assets/images/fashion.png';
+
+
 const Catagories = () =>{
     return(
-        <h1>Catagory</h1>
+        <>
+            <div className="testDiv">
+                <div className="catagoryImgdiv">
+                    <img className="catagoryImg" src={mobileimg}  alt="mobile"/>
+                </div>
+                <div className=" catagoryImgdiv">
+                    <img className="catagoryImg" src={tvimg}  alt="tvimg"/>
+                </div>
+                <div className="catagoryImgdiv">
+                    <img className="catagoryImg" src={laptopimg}  alt="laptop"/>
+                </div>
+                <div className="catagoryImgdiv">
+                    <img className="catagoryImg" src={fridge}  alt="fridge"/>
+                </div>
+                <div className="catagoryImgdiv">
+                    <img className="catagoryImg" src={fashionimg}  alt="fashionimg"/>
+                </div>
+                <div className="catagoryImgdiv">
+                    <img className="catagoryImg" src={fashionimg}  alt="fashionimg"/>
+                </div> <div className="catagoryImgdiv">
+                    <img className="catagoryImg" src={fashionimg}  alt="fashionimg"/>
+                </div> <div className="catagoryImgdiv">
+                    <img className="catagoryImg" src={fashionimg}  alt="fashionimg"/>
+                </div>
+
+            </div>
+        </>
     )
 }
 
